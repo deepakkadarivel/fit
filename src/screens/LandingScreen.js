22f72da553ab85a19e7c2a7ed9e9d02964a2ba18
deepakkadarivel <deepak.kadarivel@gmail.com>
2018-06-21 00:49:51 +0530

    [FIT-017] Animation on Logo, App title and question of Landing page.

--- src/screens/LandingScreen.js
+++ src/screens/LandingScreen.js
@@ -1,21 +1,76 @@
-import React from "react";
-import {Image} from "react-native";
+import React, {Component} from "react";
+import {Animated} from "react-native";
 import Background from "../components/Background/Background";
 import constants from "../shared/constants";
 import GoalsListContainer from "../components/GoalsList/GoalsListContainer";
 import CustomText from "../components/CustomText/CustomText";
 import textStyle from "../components/CustomText/customTextStyleSheet";
 
-const LandingScreen = (props) => {
-    return (
-        <Background isLandingScreen={true}>
-            <Image source={require("../assets/icon8Logo.png")}/>
-            <CustomText text={constants.APP_WELCOME_TEXT} style={textStyle.appTitle}/>
-            <CustomText text={constants.QUESTION_GOAL} style={textStyle.question}/>
-            <GoalsListContainer navigation={props.navigation}/>
-        </Background>
-    );
-};
+class LandingScreen extends Component {
+    state = {
+        logoAnimation: new Animated.Value(250),
+        textOpacity: new Animated.Value(0),
+        textTransform: new Animated.Value(10),
+    };
+
+    startAnimation = () => {
+
+        Animated.sequence([
+            Animated.timing(this.state.logoAnimation, {
+                toValue: 0,
+                duration: 1000,
+            }),
+            Animated.parallel([
+                Animated.timing(this.state.textOpacity, {
+                    toValue: 1,
+                    duration: 500,
+                }),
+                Animated.timing(this.state.textTransform, {
+                    toValue: 0,
+                    duration: 500,
+                }),
+                Animated.delay(1000)
+            ]),
+        ]).start();
+
+    };
+
+    componentWillMount() {
+        this.startAnimation();
+    }
+
+    render() {
+        const logoAnimatedStyle = {
+            transform: [
+                {
+                    translateY: this.state.logoAnimation,
+                }
+            ]
+        };
+
+        const welcomeTextStyle = {
+            opacity: this.state.textOpacity,
+            transform: [
+                {
+                    translateY: this.state.textTransform,
+                }
+            ]
+        };
+
+        return (
+            <Background isLandingScreen={true}>
+                <Animated.Image source={require("../assets/icon8Logo.png")} style={logoAnimatedStyle}/>
+                <Animated.View style={welcomeTextStyle}>
+                    <CustomText text={constants.APP_WELCOME_TEXT} style={textStyle.appTitle}/>
+                </Animated.View>
+                <Animated.View style={welcomeTextStyle}>
+                    <CustomText text={constants.QUESTION_GOAL} style={textStyle.question}/>
+                </Animated.View>
+                <GoalsListContainer navigation={this.props.navigation}/>
+            </Background>
+        );
+    }
+}
 
 LandingScreen.navigationOptions = {
     header: null
